cef6b312421fb6fdd1b4e59ed7d09353ca19181e
bmmcc4 <bmflight20@gmail.com>
2022-02-18 18:56:29 -0800

    Refactored Example

--- map_type_picker/example/lib/main.dart
+++ map_type_picker/example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:example/screens/homepagescreen.dart';
+import 'package:map_type_picker/map_type_picker.dart';
+import 'package:platform_maps_flutter/platform_maps_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,5 +19,44 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    MapTypePicker.mapType = MapType.normal; // Set Default Map Type here
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Sample Map Type Picker")),
+      body: Stack(children: [
+        PlatformMap(
+            mapType: MapTypePicker.mapType, // Enter Map Type Here
+            initialCameraPosition: const CameraPosition(
+              target: LatLng(51.507391, -0.1277),
+              zoom: 10.0,
+            )),
+        MapTypePicker(
+            notifyParent: refresh,
+            edgeInsetsAll: 10,
+            backgroundColor: Colors.grey.withOpacity(0.45)) // Overlay Here
+      ]),
+    );
+  }
+
+  // refresh to update parent state from child
+  refresh() {
+    setState(() {});
   }
 }
